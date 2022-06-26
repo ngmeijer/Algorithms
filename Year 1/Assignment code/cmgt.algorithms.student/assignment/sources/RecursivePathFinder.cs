71ed10a0e14c878f29817ed1c853c0ae5724fbdd
Nils <meijernils9@gmail.com>
2022-06-26 17:11:55 +0200

    Working on moving back up the recursion tree after finding final node.

--- Year 1/Assignment code/cmgt.algorithms.student/assignment/sources/RecursivePathFinder.cs
+++ Year 1/Assignment code/cmgt.algorithms.student/assignment/sources/RecursivePathFinder.cs
@@ -5,15 +5,12 @@ using System.Collections.Generic;
 internal class RecursivePathFinder : PathFinder
 {
     private List<Node> visitedNodes = new List<Node>();
-    private List<Node> toDo = new List<Node>();
     private List<Node> currentNodesInPath = new List<Node>();
-    private Node startNode;
     private Node endNode;
     private Node lastNode;
-    private bool foundFinalNode;
+    private bool foundTargetNode;
     private bool foundNextNode = false;
     private int iterationCount = 0;
-    private int indexOfCurrentNode;
     private bool finishLoop;
 
     public RecursivePathFinder(NodeGraph pGraph, NodeGraphAgent pAgent) : base(pGraph, pAgent)
@@ -22,7 +19,7 @@ internal class RecursivePathFinder : PathFinder
 
     protected override List<Node> generate(Node pFrom, Node pTo)
     {
-        startNode = pFrom;
+        Node startNode = pFrom;
         endNode = pTo;
 
         iterationCount = 0;
@@ -52,50 +49,57 @@ internal class RecursivePathFinder : PathFinder
 
         foundNextNode = false;
 
-        checkIfIsTargetNode(pNode);
-
         //Adding node to path & already visited nodes.
+        Console.WriteLine($"Adding {pNode.id} to path & visitedNodes.");
+        lastNode = currentNodesInPath[currentNodesInPath.Count - 1];
         if (!currentNodesInPath.Contains(pNode)) currentNodesInPath.Add(pNode);
         if (!visitedNodes.Contains(pNode)) visitedNodes.Add(pNode);
 
-        //If the path contains more than 1 node, set lastNode to the node added BEFORE currentNode.
-        if (currentNodesInPath.Count > 1) lastNode = currentNodesInPath[currentNodesInPath.Count - 2];
+        foundTargetNode = checkIfIsTargetNode(pNode);
 
-        //Check connection so
+        if (foundTargetNode)
+        {
+            moveToLastNode(pNode);
+            return;
+        }
+
+        //Check connections
         loopOverConnections(pNode);
 
         //If all children have been visited but a next node has not been found, remove node from path. Retrace path to last node and loopOverConnections().
-
-        if (!foundNextNode && !finishLoop && lastNode != null)
+        if (!foundNextNode)
         {
-            currentNodesInPath.Remove(pNode);
-            loopOverConnections(lastNode);
+            Console.WriteLine($"Haven't found an unvisited connection for {pNode.id}. Returning to last node {lastNode.id}.");
+            moveToLastNode(pNode);
         }
     }
 
-    private static void printConnections(Node pNode)
+    private void printConnections(Node pNode)
     {
-        Console.WriteLine($"\nCurrently investigating {pNode.id}");
-        Console.WriteLine($"Node has connections:");
+        Console.WriteLine($"\nInvestigating node {pNode.id}");
         foreach (Node connection in pNode.connections)
         {
-            Console.WriteLine($"{connection.id}");
+            Console.WriteLine($"- connection {connection.id}. Visited? {visitedNodes.Contains(connection)}");
         }
     }
 
-    private void checkIfIsTargetNode(Node pNode)
+    private bool checkIfIsTargetNode(Node pNode)
     {
         //If final node is found, save path. Copy path to a new instance.
         if (pNode == endNode)
         {
-            Console.WriteLine($"\n--------- FOUND FINAL NODE {pNode.id} -----------\n");
+            Console.WriteLine($"\n--------- FOUND FINAL NODE {pNode.id} -----------\n" +
+                                $"--------- Saving path.        -----------");
             currentNodesInPath.Add(pNode);
             visitedNodes.Add(pNode);
-            foundFinalNode = true;
 
             savePath(currentNodesInPath);
             currentNodesInPath = new List<Node>(currentNodesInPath);
+
+            return true;
         }
+
+        return false;
     }
 
     private void loopOverConnections(Node pNode)
@@ -104,37 +108,38 @@ internal class RecursivePathFinder : PathFinder
         {
             if (finishLoop) break;
 
-            if (visitedNodes.Contains(connection) || pNode.alreadyVisited.Contains(connection))
+            if (connection != endNode)
             {
-                continue;
+                if (visitedNodes.Contains(connection) || pNode.alreadyVisited.Contains(connection))
+                {
+                    Console.WriteLine($"Skipping connection {connection.id}. Already visited.");
+                    continue;
+                }
             }
+            else Console.WriteLine($"");
 
             pNode.alreadyVisited.Add(connection);
             foundNextNode = true;
+            Console.WriteLine($"Selected node {connection.id} to check out.");
             findPath(connection);
         }
     }
 
-
-
     private void moveToLastNode(Node pNode)
     {
+        if(pNode == lastNode)
+        {
+            int indexOfpNode = currentNodesInPath.IndexOf(pNode);
+            Console.WriteLine($"Last node {lastNode.id} is the same as pNode. Is pNode {pNode.id} in path? {currentNodesInPath.Contains(pNode)}. Index of pNode in path: {indexOfpNode}");
+        }
         //If the algorithm has run less than X times, find previous node and call recursive.
         if (iterationCount < AlgorithmsAssignment.MAX_PATH_ITERATION_COUNT)
         {
             iterationCount++;
 
-            int currentNodeIndex = currentNodesInPath.IndexOf(pNode);
-            Node previousNode = null;
-            if (currentNodeIndex < 0)
-            {
-                Console.WriteLine($"Couldn't find previous node of {pNode.id} in currentNodesInPath.");
-                return;
-            }
-            previousNode = currentNodesInPath[currentNodeIndex - 1];
-            Console.WriteLine($"Current node: {pNode.id}. Previous node: {previousNode.id}");
-            currentNodesInPath.Remove(pNode);
-            findPath(previousNode);
+            Console.WriteLine($"Current node: {pNode.id} (removing from path). Retracing further to {lastNode.id}");
+            currentNodesInPath.RemoveAt(currentNodesInPath.Count - 1);
+            loopOverConnections(lastNode);
         }
         else finishLoop = true;
     }
