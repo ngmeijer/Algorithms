015c90fb105054b12dd0dc8f2cacdb821ddac609
Nils Meijer <meijernils9@gmail.com>
2022-07-07 14:40:33 +0200

    Improving PathFinderAgent

--- Year 1/Assignment code/cmgt.algorithms.student/assignment/sources/Assignment/Agent/PathFindingAgent.cs
+++ Year 1/Assignment code/cmgt.algorithms.student/assignment/sources/Assignment/Agent/PathFindingAgent.cs
@@ -5,47 +5,45 @@ using System.Linq;
 
 internal class PathFindingAgent : NodeGraphAgent
 {
-	private PathFinder pathFinder;
+    private PathFinder pathFinder;
 
-	public PathFindingAgent(NodeGraph pGraph, PathFinder pPathFinder) : base(pGraph)
+    public PathFindingAgent(NodeGraph pGraph, PathFinder pPathFinder) : base(pGraph)
     {
-		pathFinder = pPathFinder;
+        pathFinder = pPathFinder;
 
-		//position ourselves on a random node
-		if (pGraph.nodes.Count > 0)
-		{
-			List<Node> allNodes = pGraph.nodes.Values.ToList();
+        //position ourselves on a random node
+        if (pGraph.nodes.Count > 0)
+        {
+            List<Node> allNodes = pGraph.nodes.Values.ToList();
 
-			Node randomNode = allNodes[Utils.Random(0, allNodes.Count)];
-			currentNode = randomNode;
-			jumpToNode(randomNode);
-		}
+            Node randomNode = allNodes[Utils.Random(0, allNodes.Count)];
+            currentNode = randomNode;
+            jumpToNode(randomNode);
+        }
 
-		pGraph.OnNodeLeftClicked += onNodeClickHandler;
-	}
+        pGraph.OnNodeLeftClicked += onNodeClickHandler;
+    }
 
-	protected virtual void onNodeClickHandler(Node pNode)
-	{
-		List<Node> nodes = pathFinder.Generate(currentNode, pNode);
-		nodePath = new Queue<Node>(nodes);
-	}
+    protected virtual void onNodeClickHandler(Node pNode)
+    {
+        List<Node> nodes = pathFinder.Generate(currentNode, pNode);
+        nodePath = new Queue<Node>(nodes);
+    }
 
-	protected override void Update()
-	{
-		if (nodePath.Count > 0)
-		{
-			if (targetNode == null)
-			{
-				targetNode = nodePath.Peek();
-			}
-		}
-		else return;
+    protected override void Update()
+    {
+        if (nodePath.Count > 0)
+        {
+            if (targetNode == null)
+                targetNode = nodePath.Peek();
+        }
+        else return;
 
-		if (moveTowardsNode(targetNode))
-		{
-			currentNode = targetNode;
-			targetNode = null;
-			nodePath.Dequeue();
-		}
-	}
+        if (moveTowardsNode(targetNode))
+        {
+            currentNode = targetNode;
+            targetNode = null;
+            nodePath.Dequeue();
+        }
+    }
 }
